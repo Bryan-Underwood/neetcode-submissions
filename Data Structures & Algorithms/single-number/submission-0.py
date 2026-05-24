522c1ebf71001d44b967e7396303bc6332b2dca1
Bryan Underwood <btu3410@gmail.com>
2026-05-23 19:27:54 -0500

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        a = 0
+
+        for x in nums:
+            a ^= x
+
+        return a
